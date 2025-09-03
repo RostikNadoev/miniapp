c034666b7a938b2b506c95fa0a9c72d92dafdcff
Mike <rostikcool@mail.ru>
2025-09-03 23:12:04 +0300

    new

--- app.js
+++
@@ -1,7 +0,0 @@
-// Telegram WebApp API доступна глобально
-const tg = window.Telegram.WebApp;
-
-tg.expand(); // расширить окно
-tg.ready(); // мини-приложение готово
-
-document.getElementById("greeting").innerText = "Привет из Mini App!";
